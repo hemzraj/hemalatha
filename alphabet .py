771e7dad9288df8f61692e70cb012f143ba7a532
hemzraj <37288181+hemzraj@users.noreply.github.com>
2019-02-05 11:35:54 +0530

    Create alphabet .py

---
+++ alphabet .py
@@ -0,0 +1,6 @@
+# taking user input
+ch = input("Enter a character: ")
+if((ch>='A' and ch<= 'Z') or (ch>='a' and ch<='z')):
+    print(ch, "is an Alphabet")
+else:
+    print(ch, "is not an Alphabet")
