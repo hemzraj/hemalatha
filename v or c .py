5567a70e5ba26189a0f1793706e3825de002df5b
hemzraj <37288181+hemzraj@users.noreply.github.com>
2019-02-05 11:16:55 +0530

    Create v or c .py

---
+++ v or c .py
@@ -0,0 +1,6 @@
+# taking user input
+ch = input("enter a character:")
+if(ch =='A'or ch =='a' or ch =='E' or ch =='e' or ch =='I' or ch =='i' ch =='O' or ch =='o' or ch =='U' or ch =='u'):
+   print(ch, "is a vowel")
+   else:
+   print(ch, "is a consonant")
