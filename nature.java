248016c9b0fca212343a1b5c021b7c61c56a405e
hemzraj <37288181+hemzraj@users.noreply.github.com>
2019-02-05 12:29:35 +0530

    Create nature.java

---
+++ nature.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Natural
+{
+	
+	public static void main(String args[])
+	{
+		int s=0;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int j=1;j<=n;j++)
+		s=s+j;
+		System.out.println(s);
+	}
+}
+	
