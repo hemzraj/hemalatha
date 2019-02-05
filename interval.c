14521755016ceb080133d2b2a1a338b5c8465d44
hemzraj <37288181+hemzraj@users.noreply.github.com>
2019-02-05 15:31:17 +0530

    Create interval.c

---
+++ interval.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+
+{
+
+    int x,y,num;
+    printf("enter the x,y value");
+    scanf("%d%d",&x,&y);
+    {
+        for(i=x;i<=y,i++)
+        {
+            if(i%2==1)
+            printf("print the i value%d",i);
+        }
+    }
+
+}
